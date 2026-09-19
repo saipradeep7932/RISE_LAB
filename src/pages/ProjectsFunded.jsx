ab57c36cdd767ced263f4701872ee9a218c2f241
riselab <riselab@iitrpr.ac.in>
2026-09-19 11:35:05 +0530

    Update CO-Pi

--- src/pages/ProjectsFunded.jsx
+++ src/pages/ProjectsFunded.jsx
@@ -24,8 +24,8 @@ const ProjectsFunded = () => {
       agency: "Ministry of Steel, Gov. of India",
       amount: "₹405,25,872/-",
       duration: "Oct-25 – Sept-27",
-      role: "Co-Principal Investigator (Co-P.I)",
-      roleOverwrite: "Co-Principal Investigator (Co-P.I)",
+      role: "Co-Principal Investigator (Co-PI)",
+      roleOverwrite: "Co-Principal Investigator (Co-PI)",
       remark: "Active"
     },
     {
